35da0a9085f94c4d97e3558ee93630d4b646853b
mayad-mohamed <mmyada7@gmail.com>
2021-03-25 11:50:53 +0200

    deployback

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-var env = process.env.NODE_ENV || 'development';
+// var env = process.env.NODE_ENV || 'development';
 
-var config = require('./config.json');
-var envConfig = config[env];
+// var config = require('./config.json');
+// var envConfig = config[env];
 
-Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+// Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
 
 
 
